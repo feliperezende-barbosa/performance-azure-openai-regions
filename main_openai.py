1a75e65db24b11d7e64a2fe5d56b358acb2335cf
Richardson Nascimento <richardsonbq@gmail.com>
2023-07-27 09:48:27 -0300

    Small changes in the code to simplify it, add documentation and translate it to english

--- main_openai.py
+++ main_openai.py
@@ -1,15 +1,27 @@
-import os
-import time
-import logging
+import json, os, time, logging
+import ask_openai as ask
 from dotenv import load_dotenv
 
+#Loading environment variables
 load_dotenv()
 
-import ask_openai as ask
+#Initialize Azure OpenAI parameters. Most of these parameters are defined in the .env file
+api_type='azure'  
+api_version = os.environ.get("API_VERSION")
+max_tokens = int(os.environ.get("AZURE_OPENAI_MAXTOKENS"))
+temperature = float(os.environ.get("AZURE_OPENAI_TEMPERATURE"))
+timeout = int(os.environ.get("AZURE_OPENAI_TIMEOUT"))
+az_openai_regions = os.environ.get("AZURE_OPENAI_REGIONS")
+regions = json.loads(az_openai_regions) 
 
+
+#Number of runs to be executed. Adjust according to your needs
+RUNS = 2
+
+#Define a default system message. You can adjust this message to your needs depending on the test type
 system_message = {"role":"system","content":"You are an AI assistant that helps people find information."}
 
-#Textos usados como exemplos para a performance de sintetização
+#Sample messages that will be used as prompts to measure the model's response time
 samples = [  
     {"role": "user", "content": "Qual é a capital do Brasil?"},  
     {"role": "user", "content": "Como funciona a fotossíntese?"},  
@@ -26,168 +38,49 @@ samples = [
 
 #variable to get the value of the data and time of the execution
 now = time.strftime("%d-%m-%Y %H-%M-%S")
-
 log_file = now + ".log"
 
-logging.basicConfig(level=logging.INFO, filename=f"./Logs/{log_file}", filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', encoding='utf-8')
+#Init the parameters of the log file to generate a csv file (; separator)
+logging.basicConfig(level=logging.INFO, filename=f"./{log_file}", filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', encoding='utf-8')
 logging.info(f";Status;Region;Sample;Response_openai_s;Request_Duration_s;Prompt Size;Completion Size")
 
-runs = 10
+#Loop to execute the number of runs desired
+for i in range(RUNS):
+    
+    #Loop to iterate over the regions
+    for region, data in regions.items():  
+        print(f"Processing {region} region")
 
-for i in range(runs):
-    for sample in samples:
-        print(f"Processando execução #{i + 1} com sample do tipo {sample}")    
-        
-        #call eastus region
-        start = time.time()
-        response_oai_east_us = ask.AskOpenAi(os.getenv("OPENAI_API_TYPE"),
-                                            os.getenv("API_BASE_EASTUS"),
-                                            os.getenv("API_VERSION_EASTUS"),
-                                            os.getenv("API_KEY_EASTUS"),
-                                            os.getenv("DEPLOYMENT_ID_EASTUS"),
-                                            os.getenv("OPENAI_MAX_TOKENS")
-                                            ).ask(system_message, sample)
-        end = time.time()
+        #Initiate the Azure OpenAI API for the region
+        az_openai = ask.AskOpenAi(deployment_id=data["DEPLOYMENT_NAME"],  
+                                         api_base=data["ENDPOINT"],  
+                                         api_version=api_version,  
+                                         api_key=data["API_KEY"],
+                                         api_type=api_type,
+                                         max_tokens=max_tokens,
+                                         temperature=temperature,
+                                         timeout=timeout)
 
-        duration = end - start
+        #Iterate over the samples
+        for sample in samples:
+            print(f"Processing execution #{i + 1} with sample of type {sample}")
 
-        if response_oai_east_us != None:
-            #variable to get the value of converted milliseconds to seconds
-            response_ms_eastus = response_oai_east_us.response_ms / 1000
+            # Call the completion API for the sample and region and register the duration of the request
+            start = time.time()
+            
+            response = az_openai.ask(system_message, sample)  
+            
+            end = time.time()  
+      
+            duration = end - start  
 
-            tokens_completion = response_oai_east_us.usage.completion_tokens
-            tokens_prompt = response_oai_east_us.usage.prompt_tokens
+            #Check if the response succeed to log the details
+            if response != None:
+                #Log success with additional performance details 
+                logging.info(f";SUCCESS;{region};{sample};{response.response_ms/1000};{duration};{response.usage.prompt_tokens};{response.usage.completion_tokens}")  
+            else:  
+                logging.error(f";ERROR;{region};{sample};NaN;{duration};NaN;NaN")  
+  
+            time.sleep(0.5)  
 
-            logging.info(f";SUCCESS;East US;{sample};{response_ms_eastus};{duration};{tokens_prompt};{tokens_completion}")
-        else:        
-            logging.error(f";ERROR;East US;{sample};NaN;{duration};NaN;NaN")        
-        #finish eastus region
-
-        #call uksouth region
-        start = time.time()
-        response_oai_uk_south = ask.AskOpenAi(os.getenv("OPENAI_API_TYPE"),
-                                            os.getenv("API_BASE_UKSOUTH"),
-                                            os.getenv("API_VERSION_UKSOUTH"),
-                                            os.getenv("API_KEY_UKSOUTH"),
-                                            os.getenv("DEPLOYMENT_ID_UKSOUTH"),
-                                            os.getenv("OPENAI_MAX_TOKENS")
-                                            ).ask(system_message, sample)
-        end = time.time()
-
-        duration = end - start
-
-        if response_oai_uk_south != None:
-            #variable to get the value of converted milliseconds to seconds
-            response_ms_uksouth = response_oai_uk_south.response_ms / 1000
-
-            tokens_completion = response_oai_uk_south.usage.completion_tokens
-            tokens_prompt = response_oai_uk_south.usage.prompt_tokens
-
-            logging.info(f";SUCCESS;UK South;{sample};{response_ms_uksouth};{duration};{tokens_prompt};{tokens_completion}")
-        else:
-            logging.error(f";ERROR;UK South;{sample};NaN;{duration};NaN;NaN")        
-        #finish uksouth region
-
-        #call francecentral region
-        start = time.time()
-        response_oai_france_central = ask.AskOpenAi(os.getenv("OPENAI_API_TYPE"),
-                                                    os.getenv("API_BASE_FRANCECENTRAL"),
-                                                    os.getenv("API_VERSION_FRANCECENTRAL"),
-                                                    os.getenv("API_KEY_FRANCECENTRAL"),
-                                                    os.getenv("DEPLOYMENT_ID_FRANCECENTRAL"),
-                                                    os.getenv("OPENAI_MAX_TOKENS")
-                                                    ).ask(system_message, sample)
-        end = time.time()
-
-        duration = end - start
-
-        if response_oai_france_central != None:
-            #variable to get the value of converted milliseconds to seconds
-            response_ms_francen = response_oai_france_central.response_ms / 1000
-
-            tokens_completion = response_oai_france_central.usage.completion_tokens
-            tokens_prompt = response_oai_france_central.usage.prompt_tokens
-
-            logging.info(f";SUCCESS;France Central;{sample};{response_ms_francen};{duration};{tokens_prompt};{tokens_completion}")
-        else:
-            logging.error(f";ERROR;France Central;{sample};NaN;{duration};NaN;NaN")
-        #finish francecentral region
-
-        #call canadaeast region
-        start = time.time()
-        response_oai_canada_east = ask.AskOpenAi(os.getenv("OPENAI_API_TYPE"),
-                                                os.getenv("API_BASE_CANADAEAST"),
-                                                os.getenv("API_VERSION_CANADAEAST"),
-                                                os.getenv("API_KEY_CANADAEAST"),
-                                                os.getenv("DEPLOYMENT_ID_CANADAEAST"),
-                                                os.getenv("OPENAI_MAX_TOKENS")
-                                                ).ask(system_message, sample)
-        end = time.time()
-
-        duration = end - start
-
-        if response_oai_canada_east != None:
-            #variable to get the value of converted milliseconds to seconds
-            response_ms_canadaeast = response_oai_canada_east.response_ms / 1000
-
-            tokens_completion = response_oai_canada_east.usage.completion_tokens
-            tokens_prompt = response_oai_canada_east.usage.prompt_tokens
-
-            logging.info(f";SUCCESS;Canada East;{sample};{response_ms_canadaeast};{duration};{tokens_prompt};{tokens_completion}")
-        else:
-            logging.error(f";ERROR;Canada East;{sample};NaN;{duration};NaN;NaN")
-        #finish canadaeast region
-
-        #call northcentralus region
-        start = time.time()
-        response_oai_northcentralus = ask.AskOpenAi(os.getenv("OPENAI_API_TYPE"),
-                                                    os.getenv("API_BASE_NORTHCENTRALUS"),
-                                                    os.getenv("API_VERSION_NORTHCENTRALUS"),
-                                                    os.getenv("API_KEY_NORTHCENTRALUS"),
-                                                    os.getenv("DEPLOYMENT_ID_NORTHCENTRALUS"),
-                                                    os.getenv("OPENAI_MAX_TOKENS")
-                                                    ).ask(system_message, sample)
-        end = time.time()
-
-        duration = end - start
-
-        if response_oai_northcentralus != None:
-            #variable to get the value of converted milliseconds to seconds
-            response_ms_northcentralus = response_oai_northcentralus.response_ms / 1000
-
-            tokens_completion = response_oai_northcentralus.usage.completion_tokens
-            tokens_prompt = response_oai_northcentralus.usage.prompt_tokens
-
-            logging.info(f";SUCCESS;North Central US;{sample};{response_ms_northcentralus};{duration};{tokens_prompt};{tokens_completion}")
-        else:
-            logging.error(f";ERROR;North Central US;{sample};NaN;{duration};NaN;NaN")
-        #finish northcentralus region
-        
-        #call westeurope region
-        start = time.time()
-        response_oai_westeurope = ask.AskOpenAi(os.getenv("OPENAI_API_TYPE"),
-                                                os.getenv("API_BASE_WESTEUROPE"),
-                                                os.getenv("API_VERSION_WESTEUROPE"),
-                                                os.getenv("API_KEY_WESTEUROPE"),
-                                                os.getenv("DEPLOYMENT_ID_WESTEUROPE"),
-                                                os.getenv("OPENAI_MAX_TOKENS")
-                                                ).ask(system_message, sample)
-        end = time.time()
-
-        duration = end - start
-
-        if response_oai_westeurope != None:
-            #variable to get the value of converted milliseconds to seconds
-            response_ms_westeurope = response_oai_westeurope.response_ms / 1000
-
-            tokens_completion = response_oai_westeurope.usage.completion_tokens
-            tokens_prompt = response_oai_westeurope.usage.prompt_tokens
-
-            logging.info(f";SUCCESS;West Europe;{sample};{response_ms_westeurope};{duration};{tokens_prompt};{tokens_completion}")
-        else:
-            logging.error(f";ERROR;West Europe;{sample};NaN;{duration};NaN;NaN")
-        #finish westeurope region
-        
-        time.sleep(1)
-
-print("Processo finalizado.")
+print("Process completed.")
